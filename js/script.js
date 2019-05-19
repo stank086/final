a87a98585355a96e4fab30b394c33f2f1867905a
stank086 <47401131+stank086@users.noreply.github.com>
2019-05-18 21:05:57 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	$(".gallery").slick({ // enables slick library to .gallery div
+		dots: true, // enables pagination buttons
+		fade: true	// enables fade transitions between pictures
+	});
+
+	$(".img3").slick({ // enables slick library to .gallery div
+		dots: true, // enables pagination buttons
+		fade: true	// enables fade transitions between pictures
+	});
+
+	$(".gallery2").slick({ // enables slick library to .gallery div
+		dots: true, // enables pagination buttons
+		fade: true	// enables fade transitions between pictures
+	});
+});
